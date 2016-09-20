10e67b6caa5b14813a50870425f99b33210f343a
Robb Lovell <robblovell@gmail.com>
2016-09-20 16:24:37 -0700

    react-native upgrade. uses gradle 2.2.0

--- android/app/src/main/java/in/liive/MainActivity.java
+++ android/app/src/main/java/in/liive/MainActivity.java
@@ -1,6 +1,8 @@
 package in.liive;
 
 import com.facebook.react.ReactActivity;
+import com.oney.WebRTCModule.WebRTCModulePackage;
+//import com.microsoft.codepush.react.CodePush;
 import com.oblador.vectoricons.VectorIconsPackage;
 
 public class MainActivity extends ReactActivity {
