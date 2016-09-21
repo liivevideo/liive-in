6818d75957a4924d21e83ecbb3ebd130edb3beda
Robb Lovell <robblovell@gmail.com>
2016-09-20 20:16:34 -0700

    git variables made available in build.

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -7,3 +7,10 @@ else
    rm -f package.json
    cp -f package-native.json package.json
 fi
+# Example: Expose the commit SHA accessible through $GIT_REVISION_SHA Environment Variable
+export GIT_REVISION_SHA=$(git rev-parse HEAD)
+
+# Example: Expose the commit author & email through the $GIT_REVISION_AUTHOR in the following format: Author Name <author@example.com>
+export GIT_REVISION_AUTHOR=$(git log -1 --pretty=format:"%an <%ae>")
+echo "revision: $GIT_REVISION_SHA"
+echo "author: $GIT_REVISION_AUTHOR"
